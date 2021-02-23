0a5bfa677297910c58d61f36e8bc307b7c3ac3b7
Сергей <Seggiy@mail.ru>
2021-02-23 16:19:35 +0500

    Применение функций в циклических конструкциях Заготовка ко всем заданиям. Задание 1-4 сделаны

--- def_loop.py
+++ def_loop.py
@@ -43,12 +43,13 @@ def ex2():
 
 
 def ex3():
-    turtle.penup()
     turtle.setx(-300)
     n = 20
-    size = 30
+    size = 50
     angle1 = math.atan(1 / 2) * 180 / math.pi
     dig1 = (size ** 2 + (size / 2) ** 2) ** 0.5
+    dig2 = size * 2**0.5
+    angle2 = math.atan(size / 2 / dig2) * 180 / math.pi
 
     def square(side):
         turtle.pendown()
@@ -64,21 +65,69 @@ def ex3():
             turtle.forward(dig1)
             turtle.left(-45 - angle1)
         else:
-            turtle.left(45)
-            turtle.forward(math.sqrt(2) * size)
-            turtle.left(90)
-            turtle.backward(size / 2)
-            turtle.left(-90)
+            turtle.left(45 - angle2)
+            turtle.forward(((size / 2)**2 + dig2**2)**0.5)
+            turtle.left(angle2)
 
     turtle.done()
 
 
 def ex4():
+    n = 10
+    base = 20
+    turtle.setx(-300)
+
+    def ornament_left(size):
+        turtle.pendown()
+        small = size
+        big = size * 3 / 2
+        for angle, step in ((90, big), (90, small), (-90, small), (-90, small), (0, big)):
+            turtle.forward(step)
+            turtle.left(angle)
+        turtle.penup()
+
+    def ornament_right(size):
+        turtle.pendown()
+        small = size
+        big = size * 3 / 2
+        for angle, step in ((-90, big), (-90, small), (+90, small), (+90, small), (0, big)):
+            turtle.forward(step)
+            turtle.left(angle)
+        turtle.penup()
+
+    for i in range(n):
+        ornament_left(base)
+        ornament_right(base)
 
     turtle.done()
 
 
 def ex5():
+    n = 10
+    base = 20
+    turtle.setx(-300)
+
+    def ornament_left(size):
+        turtle.pendown()
+        small = size
+        big = size * 3 / 2
+        for angle, step in ((90, big), (90, small), (-90, small), (-90, small), (0, big)):
+            turtle.forward(step)
+            turtle.left(angle)
+        turtle.penup()
+
+    def ornament_right(size):
+        turtle.pendown()
+        small = size
+        big = size * 3 / 2
+        for angle, step in ((-90, big), (-90, small), (+90, small), (+90, small), (0, big)):
+            turtle.forward(step)
+            turtle.left(angle)
+        turtle.penup()
+
+    for i in range(n):
+        ornament_left(base)
+        ornament_right(base)
 
     turtle.done()
 
@@ -101,6 +150,7 @@ def ex8():
 def control(ex):
     turtle.hideturtle()
     turtle.speed(0)
+    turtle.penup()
     if ex == 1:
         ex1()
     elif ex == 2:
