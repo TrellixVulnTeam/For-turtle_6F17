9335f8c819552d511c2fc1022c54bea9fa452c19
Nataiel <seggiy@mail.ru>
2021-03-02 14:05:36 +0500

    Упражнения 1-21 готовы. Задание 12 и 14 переосмыслено и упрощено. Для заданий 5 и 6 реализован выбор угла орисовки зубцов. Решение задач Ветвление 3

--- Age3.py
+++ Age3.py
@@ -1,18 +1,21 @@
-age_Anton = int(input('Возраст Антона:'))
-age_Boris = int(input('Возраст Бориса:'))
-age_Victor = int(input('Возраст Виктора:'))
+def Quvest3(age_Anton, age_Boris, age_Victor):
+    if (age_Anton > age_Boris) and (age_Anton > age_Victor):
+        return 'Антон старше всех'
+    if (age_Boris > age_Anton) and (age_Boris > age_Victor):
+        print('Борис старше всех')
+    if (age_Victor > age_Anton) and (age_Victor > age_Boris):
+        print('Виктор старше всех')
+    if (age_Anton > age_Victor) and (age_Anton == age_Boris):
+        print('Антон и Борис старше Виктора.')
+    if (age_Anton > age_Boris) and (age_Anton == age_Victor):
+        print('Антон и Виктор старше Бориса.')
+    if (age_Victor > age_Anton) and (age_Victor == age_Boris):
+        print('Виктор и Борис старше Антона.')
+    if age_Anton == age_Boris == age_Boris == age_Victor:
+        print('Виктор, Борис и Антон одного возраста.')
 
-if (age_Anton > age_Boris) and (age_Anton > age_Victor):
-    print('Антон старше всех')
-if (age_Boris > age_Anton) and (age_Boris > age_Victor):
-    print('Борис старше всех')
-if (age_Victor > age_Anton) and (age_Victor > age_Boris):
-    print('Виктор старше всех')
-if (age_Anton > age_Victor) and (age_Anton == age_Boris):
-    print('Антон и Борис старше Виктора.')
-if (age_Anton > age_Boris) and (age_Anton == age_Victor):
-    print('Антон и Виктор старше Бориса.')
-if (age_Victor > age_Anton) and (age_Victor == age_Boris):
-    print('Виктор и Борис старше Антона.')
-if (age_Anton == age_Boris) and (age_Boris == age_Victor):
-    print('Виктор, Борис и Антон одного возраста.')
+tests = ((1, 2, 3),(3, 2, 1),(1, 3, 1),(2, 2, 1),(2, 1, 2),(1, 2, 2),(3, 3, 3))
+
+for i in tests:
+    if "Антон старше всех" == Quvest3(*i):
+        print(True)
