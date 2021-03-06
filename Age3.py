934b9f22e78b78ac2b4281ecebd06e2f34e28483
Nataiel <seggiy@mail.ru>
2021-03-06 14:04:19 +0500

    Упражнения 1-21 готовы. Задание 12 и 14 переосмыслено и упрощено. Для заданий 5 и 6 реализован выбор угла орисовки зубцов. Решение задач Ветвление 3 Заготовка ДЗ IF1 и IF2 (приближено к PEP8)

--- Age3.py
+++ Age3.py
@@ -1,20 +1,21 @@
-def Quvest3(age_Anton, age_Boris, age_Victor):
+def quvest3(age_Anton, age_Boris, age_Victor):
     if (age_Anton > age_Boris) and (age_Anton > age_Victor):
         print('Антон старше всех')
-    if (age_Boris > age_Anton) and (age_Boris > age_Victor):
+    elif (age_Boris > age_Anton) and (age_Boris > age_Victor):
         print('Борис старше всех')
-    if (age_Victor > age_Anton) and (age_Victor > age_Boris):
+    elif (age_Victor > age_Anton) and (age_Victor > age_Boris):
         print('Виктор старше всех')
-    if (age_Anton > age_Victor) and (age_Anton == age_Boris):
+    elif (age_Anton > age_Victor) and (age_Anton == age_Boris):
         print('Антон и Борис старше Виктора.')
-    if (age_Anton > age_Boris) and (age_Anton == age_Victor):
+    elif (age_Anton > age_Boris) and (age_Anton == age_Victor):
         print('Антон и Виктор старше Бориса.')
-    if (age_Victor > age_Anton) and (age_Victor == age_Boris):
+    elif (age_Victor > age_Anton) and (age_Victor == age_Boris):
         print('Виктор и Борис старше Антона.')
-    if age_Anton == age_Boris == age_Boris == age_Victor:
+    elif age_Anton == age_Boris == age_Boris == age_Victor:
         print('Виктор, Борис и Антон одного возраста.')
 
-tests = ((1, 2, 3),(3, 2, 1),(1, 3, 1),(2, 2, 1),(2, 1, 2),(1, 2, 2),(3, 3, 3))
 
-for i in tests:
-    Quvest3(*i)
+tests = ((3, 2, 1), (2, 3, 2), (1, 1, 2), (2, 2, 1), (1, 2, 2), (2, 1, 2), (3, 3, 3))
+
+for q in tests:
+    quvest3(50, 20, 10)
