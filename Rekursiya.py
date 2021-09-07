718191c004009e3a61b735ad88e03ebd51ab23a2
Nataiel <seggiy@mail.ru>
2021-09-07 15:20:44 +0500

    Рекурсия Дерева с лисьюями

--- Rekursiya.py
+++ Rekursiya.py
@@ -1,6 +1,12 @@
 import turtle
 
 
+def circle(R):
+    turtle.left(90)
+    turtle.circle(-R)
+    turtle.right(90)
+
+
 def pascal_tree(x, a):
     """
     :param x: глубина рекурсии
@@ -11,16 +17,22 @@ def pascal_tree(x, a):
     if x > 0:
         turtle.left(45)
         pascal_tree(x-1, a * 0.75)
-        turtle.right(90)
+        turtle.right(45)
+        pascal_tree(x - 1, a * 0.75)
+        turtle.right(45)
         pascal_tree(x - 1, a * 0.75)
         turtle.left(45)
+    if x ==0:
+        circle(a//4)
     turtle.backward(a)
 
 
 turtle.left(90)
-n = 10
-size = 300
+n = 5
+size = 200
 turtle.sety(-300)
+turtle.speed(0)
 pascal_tree(n, size)
+
 
 turtle.done()
