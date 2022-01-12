be9afb456d4e8be2b343f7848f908b4adbeb6545
Сергей <Seggiy@mail.ru>
2022-01-12 11:55:07 +0500

    Просмотр 11 класс главы 6

--- 11 класс/Глава 6. Алгоритмизация/11-stack-postfix.py
+++ 11 класс/Глава 6. Алгоритмизация/11-stack-postfix.py
@@ -9,20 +9,24 @@
  Результат: 
    2
 """
-#s = input("Введите выражение в постфиксной записи: ")
+# s = input("Введите выражение в постфиксной записи: ")
 s = "5 15 + 4 7 + 1 - /"
-data = s.split()				
+data = s.split()
 
-stack = []					
-for x in data:					
-  if x in "+-*/":				
-    op2 = int(stack.pop())			
-    op1 = int(stack.pop())			
-    if   x == "+": res = op1 + op2		
-    elif x == "-": res = op1 - op2		
-    elif x == "*": res = op1 * op2		
-    else:         res = op1 // op2		
-    stack.append ( res )			
-  else:
-    stack.append ( x )				
-print ( stack[0] )					
+stack = []
+for x in data:
+    if x in "+-*/":
+        op2 = int(stack.pop())
+        op1 = int(stack.pop())
+        if x == "+":
+            res = op1 + op2
+        elif x == "-":
+            res = op1 - op2
+        elif x == "*":
+            res = op1 * op2
+        else:
+            res = op1 // op2
+        stack.append(res)
+    else:
+        stack.append(x)
+print(stack[0])
